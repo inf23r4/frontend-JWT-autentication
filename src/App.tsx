6d8728587a35d24ec319b2c80999ca93efab0ede
inf23r4 <diazagueromario@gmail.com>
2021-09-09 22:14:22 -0300

    add code-gen-apollo

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { gql, useQuery } from '@apollo/client';
+
 
 function App() {
+  const { loading, data } = useQuery(gql`
+  query {
+    hello
+  }
+`);
+
+  if (loading){ 
+    return <div>loading...</div>
+  }
+    
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <div>{JSON.stringify(data)}</div>
   );
 }
 
